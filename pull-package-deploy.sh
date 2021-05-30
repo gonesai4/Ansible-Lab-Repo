48c356fdab8525df01fbc9721164601eb4f562e9
dev1 <dev1@gmail.com>
2021-05-30 10:05:38 +0000

    fourht

--- pull-package-deploy.sh
+++ pull-package-deploy.sh
@@ -4,5 +4,5 @@ sudo chown -R centos:centos webapp
 cd webapp
 mvn clean package
 cd target
-scp webapp.war centos@172.31.29.84:/opt/webserver/webapps
+scp webapp.war centos@172.31.29.84:/opt/apache-tomcat-8.5.42/webapps
 

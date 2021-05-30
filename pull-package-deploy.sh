5202cc0e49a7370adcb9ceb6c18e0c3576435cd5
dev1 <dev1@gmail.com>
2021-05-30 15:25:45 +0000

    permission issue

--- pull-package-deploy.sh
+++ pull-package-deploy.sh
@@ -5,4 +5,6 @@ cd script/webapp
 export PATH=/opt/maven/apache-maven-3.8.1/bin:$PATH
 mvn clean package
 cd target
-scp webapp.war centos@172.31.29.84:/opt/apache-tomcat-8.5.42/webapps 
+scp webapp.war centos@172.31.29.84:/tmp
+ssh centos@172.31.29.84
+sudo mv /tmp/webapp.war /opt/apache-tomcat-8.5.42/webapps 

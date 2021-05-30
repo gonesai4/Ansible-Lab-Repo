32cd11b62b0ac476c5fff7a772020b7958c3f041
dev1 <dev1@gmail.com>
2021-05-30 13:58:57 +0000

    seventh

--- pull-package-deploy.sh
+++ pull-package-deploy.sh
@@ -2,7 +2,7 @@
 cd /opt/script
 sudo chown -R centos:centos webapp
 cd webapp
-mvn clean package
+sudo mvn clean package
 cd target
 scp webapp.war centos@172.31.29.84:/opt/apache-tomcat-8.5.42/webapps
 

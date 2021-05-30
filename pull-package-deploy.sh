46a813e0197743e1646ab089d0b2082b110bf975
dev1 <dev1@gmail.com>
2021-05-30 15:31:34 +0000

    h

--- pull-package-deploy.sh
+++ pull-package-deploy.sh
@@ -6,5 +6,3 @@ export PATH=/opt/maven/apache-maven-3.8.1/bin:$PATH
 mvn clean package
 cd target
 sudo scp webapp.war centos@172.31.29.84:/tmp
-ssh centos@172.31.29.84
-sudo mv /tmp/webapp.war /opt/apache-tomcat-8.5.42/webapps 

ae8cdec23c63c949d6ca399d5caa257e7535d457
dev1 <dev1@gmail.com>
2021-05-31 18:06:18 +0000

    c

--- push-to-tomcat.sh
+++ push-to-tomcat.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd /opt/script/webapp
-sudo chown -R centos:centos target
-cd target
-/bin/scp webapp.war centos@172.31.29.84:/tmp
+cd /opt/script/webapp/target
+scp webapp.war centos@172.31.29.84:/tmp
